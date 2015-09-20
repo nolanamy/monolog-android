68634d681e48b12bfbce562b7da423bc488fcb19
Nolan Amy <nolan@plickers.com>
2015-09-20 05:36:26 -0700

    Add search bar

--- app/src/main/java/com/monologgr/MonologgrActivity.java
+++ app/src/main/java/com/monologgr/MonologgrActivity.java
@@ -1,15 +1,51 @@
 package com.monologgr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MonologgrActivity extends AppCompatActivity {
+
+    private SearchView searchView;
+    private MenuItem   searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startService(new Intent(this, AudioRecorderService.class));
-        finish();
+        setContentView(R.layout.activity_monologgr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_monologgr, menu);
+
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String searchText) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String searchText) {
+                Toast.makeText(getApplicationContext(), "Searching for: " + searchText, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        return true;
     }
 }
